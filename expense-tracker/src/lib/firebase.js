4d4f18730bb28bc56e86a1b28954460e448a3387
momarinho <117489486+momarinho@users.noreply.github.com>
2023-02-04 16:54:09 -0300

    Update firebase.js

--- expense-tracker/src/lib/firebase.js
+++ expense-tracker/src/lib/firebase.js
@@ -10,14 +10,14 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBkQG0wnfGkco_w6OYPucH0T4wAa45ROoQ",
-    authDomain: "expense-tracker-431bb.firebaseapp.com",
-    projectId: "expense-tracker-431bb",
-    storageBucket: "expense-tracker-431bb.appspot.com",
-    messagingSenderId: "834128079825",
-    appId: "1:834128079825:web:a9ba5c96b2f2b225180ef0",
-    measurementId: "G-2J74HS66P7"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
